9e036e3978a007a62d82209fffe7725883f06fba
codeslack <codeslack@gmail.com>
2025-02-27 00:30:29 +0530

    commit#3

--- app/Http/Controllers/admin/ProductSizeController.php
+++ app/Http/Controllers/admin/ProductSizeController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\admin;
 
-use App\Models\Product;
+use App\Models\ProductSize;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class ProductSizeController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +34,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(ProductSize $productSize)
     {
         //
     }
@@ -42,7 +42,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(ProductSize $productSize)
     {
         //
     }
@@ -50,7 +50,7 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, ProductSize $productSize)
     {
         //
     }
@@ -58,7 +58,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(ProductSize $productSize)
     {
         //
     }
